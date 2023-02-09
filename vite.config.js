9f528e16824df090a12fb6a7c5347c217ec3c6aa
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-02-09 20:09:41 +0800

    Add vuetify-resolver plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
+//
+// import { defineConfig } from 'vite'
+// import vue from
+//
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [
+    vue(/* options */),
+    Components({
+      resolvers: [
+        // Vuetify
+        VuetifyResolver(),
+      ],
+    }),
+  ]
 })

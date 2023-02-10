04ecc2ebbb2245433c35d8abcd6530398068bf89
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-02-10 11:36:54 +0800

    Add dist

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,17 +1,11 @@
 <template>
     <v-row class="mt-16">
-      <v-col
-          v-for="n in 9"
-          :key="n"
-          class="d-flex child-flex"
-          cols="4"
-      >
+      <v-col>
         <v-img
-            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            :src="`https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vuejs-colored.svg`"
+            :lazy-src="`https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vuejs-colored.svg`"
             aspect-ratio="1"
             cover
-            class="bg-grey-lighten-2"
         >
           <template v-slot:placeholder>
             <v-row

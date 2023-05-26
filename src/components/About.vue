edf191de57476e76e6ca89320e7058fc706028dc
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-26 23:29:21 +0800

    Update "About Me" section

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,13 +5,11 @@ import {reactive} from "vue";
 const aboutProfile = reactive([
     {
         title: 'About Me',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, repudiandae!',
-        description2: 'Sed at orci non metus tristique suscipit vitae in nibh. Sedrutrum odio ac est hendrerit, at vestibulum felis condimentum.'
+        description: 'Hi, I\'m Ron Hedwig, a 3rd-year Bachelor of Science in Computer Science student at ACLC College of Iriga. On my website, I showcase my contributions to the world through GitHub. My portfolio has a Harry Potter theme that I believe you\'ll enjoy. Take a look and have a great time exploring my site!'
     },
     {
-        title: 'Education',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, repudiandae!',
-        description2: 'Sed at orci non metus tristique suscipit vitae in nibh. Sedrutrum odio ac est hendrerit, at vestibulum felis condimentum.'
+        title: 'Background',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, repudiandae!'
     }
 ]);
 
@@ -23,8 +21,7 @@ const aboutProfile = reactive([
             <div v-for="about in aboutProfile" class="col-xl-6">
                 <div class="section-half">
                     <h2 class="display-1 color-primary mb-5 harry-potter-font harry-potter-font">{{ about.title }}</h2>
-                    <p class="mb-5">{{ about.description }}</p>
-                    <p>{{ about.description2 }}</p>
+                    <p>{{ about.description }}</p>
                 </div>
             </div>
         </section>

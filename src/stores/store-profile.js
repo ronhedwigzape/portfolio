705191e5b6395dec355414706359a1ed16bda334
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-06-22 00:53:16 +0800

    Install axios and fetch data from Github API

--- src/stores/store-profile.js
+++ src/stores/store-profile.js
@@ -1,8 +1,10 @@
 import { defineStore } from 'pinia';
+import axios from "axios";
 
 export const useProfileStore = defineStore('profile', {
     state: () => ({
         component: 'Profile',
+        username: 'ronhedwigzape',
         name: 'Ron Hedwig',
         initial: 'A.',
         surname: 'Zape',
@@ -11,7 +13,8 @@ export const useProfileStore = defineStore('profile', {
             github: 'https://github.com/ronhedwigzape',
             twitter: 'https://twitter.com/ronhedwigzape',
             linkedin: 'https://www.linkedin.com/in/ron-hedwig-zape-b49062269/'
-        }
+        },
+        projects: []
     }),
 
     getters: {
@@ -19,6 +22,18 @@ export const useProfileStore = defineStore('profile', {
     },
 
     actions: {
+        async fetchRepositories() {
+            const username = this.username;
+            const response = await axios.get(`https://api.github.com/users/${username}/repos`);
 
+            this.projects = response.data.map(repo => ({
+                repo_name: repo.name,
+                repo_stars: repo.stargazers_count,
+                repo_forks: repo.forks_count,
+                repo_language: repo.language,
+                repo_license: repo.license?.name || '',
+                repo_visibility: repo.visibility
+            }));
+        }
     },
 });
